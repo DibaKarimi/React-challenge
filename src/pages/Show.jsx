12c79a96dd8a3d594314465c16cb7f9e754ab43c
Diba <72705126+DibaKarimi@users.noreply.github.com>
2022-06-23 16:24:00 +0100

    Created Show page, added Route

---
+++ src/pages/Show.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export const Show = () => {
+  return (
+    <>
+      <h1>Space X Ships</h1>
+    </>
+  );
+};
+export default Show;
